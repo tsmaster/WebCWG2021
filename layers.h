85c607d1e1b5de8c57e4b9e0348fd9678d708f5b
Dave LeCompte <tsmaster@gmail.com>
2021-09-11 15:51:17 -0700

    Stubbed in node.h

--- layers.h
+++ layers.h
@@ -2,3 +2,17 @@
 
 // layers for layered tiled procedural generation
 
+#ifndef layers_h
+#define layers_h
+
+enum class TileLayers {
+  WATER = 0,
+  CITY_CANDIDATE_LOCN = 1,
+  CITY_FINAL_LOCN = 2,
+  H2_CITY_POPULATION_DISTRIBUTION = 3,
+  CITY_POPULATION = 4,
+  H1_LOCAL_ROADS = 5,
+  H1_CROSS_TILE_ROADS = 6,
+};
+
+#endif //layers_h
--- node.h
+++ node.h
@@ -1,2 +1,72 @@
 // node.h
 
+#ifndef node_h
+#define node_h
+
+#include <vector>
+
+#include "olcPixelGameEngine.h"
+
+class NodeMgr;
+class City;
+class Coord;
+
+class Node
+{
+ public:
+  Node(int x, int y, int h, NodeMgr* nodeMgr);
+  Node(Coord c, NodeMgr* nodeMgr);
+
+  static void calcExtents(int x, int y, int h,
+			  int& outLeft, int& outBottom,
+			  int& outRight, int& outTop);
+
+  bool isOrigin() const;
+
+  void populate(int newLayer);
+
+  Coord getParentCoord();
+
+  void constrainFromNothing();
+
+  void constrainFromParent(Node& parentNode);
+
+  void constrainFromChild(Node& childNode);
+  
+  void draw();
+  void drawLabels();
+
+  void getBaseExtents();
+
+  void paveRoads();
+
+  void paveTo();
+
+  void paveCrossTileRoad(Node& neighborNode);
+
+  bool isLocnInNodesExtents(Coord coord);
+
+ private:
+  void drawRoads();
+
+  
+  NodeMgr* m_nodeMgr;
+
+  Coord* m_coord;
+  olc::Pixel m_color = olc::Pixel(0, 0, 200);
+  unsigned int m_lastAccessed;
+
+  bool m_isCity;
+  City* m_city;
+
+  std::vector<Coord> m_childCityCoords;
+  std::vector<Coord> m_connectedCities;
+  
+  // pavedLinks
+  // pavedDirs
+
+  // the layer to which this tile has been populated  
+  int m_populatedLayer; 
+};
+
+#endif // node_h
