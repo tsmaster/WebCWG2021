5fdf5b1df98108e6a909c8258b54f2254699dd9b
Dave LeCompte <tsmaster@gmail.com>
2021-09-11 16:00:54 -0700

    Stubbed in city.cpp and city.h

--- city.cpp
+++ city.cpp
@@ -1,3 +1,37 @@
 // CITY.CPP
 
 #include "city.h"
+
+
+City::City(int x, int y)
+{
+  m_coord = Coord(x, y, 0);
+  m_name = makeName();
+}
+
+std::string City::makeName()
+{
+  return std::string("???");
+
+  /*
+        random.seed(seed)
+
+        s = ""
+        length = random.randrange(4, 10)
+
+        vowels = "aeiou"
+        consonants = "bcdfghjklmnprstvwxyz"
+        allletters = vowels+consonants
+        
+        while len(s) < length:
+            if len(s) == 0:
+                s = s + random.choice(allletters)
+            elif s[-1] in vowels:
+                s = s + random.choice(consonants)
+            else:
+                s = s + random.choice(vowels)
+        return s
+  */
+
+  
+}
--- city.h
+++ city.h
@@ -1,1 +1,25 @@
 // CITY.H
+
+#ifndef city_h
+#define city_h
+
+#include <string>
+
+#include "coord.h"
+
+class City
+{
+ public:
+  City(int x, int y);
+
+  std::string getName();
+  
+ private:
+  Coord m_coord;
+  std::string makeName();
+  
+
+  std::string m_name;
+};
+
+#endif //city_h
